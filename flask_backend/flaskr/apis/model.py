55706e9f9c7b9e5e3fc71ae4586bc84ef3345e5c
WuXuan374 <2953098003@qq.com>
2021-05-16 23:41:12 +0800

    model metrics

--- flask_backend/flaskr/apis/model.py
+++ flask_backend/flaskr/apis/model.py
@@ -1,18 +1,27 @@
 from flask import Blueprint, request, make_response, jsonify
 from ..db import get_db
+import json
+from flask_restplus import fields, marshal
 
 bp = Blueprint('model', __name__, url_prefix='/model')
+resource_fields = {
+    'name': fields.String,
+    'epoch': fields.Integer,
+    'exactMatch': fields.Float,
+    'f1': fields.Float,
+    'loss': fields.Float
+}
 
 
-@bp.route('/model/evaluation/', methods=['GET'])
+
+@bp.route('/evaluation/', methods=['GET'])
 def model_evaluation():
     db = get_db()
     model_name = request.args.get("model_name")
     results = db.execute(
-        "SELECT * FROM model_evaluation WHERE name LIKE '?%'",(model_name,)
+        "SELECT * FROM model_evaluation WHERE name LIKE :search",{"search": model_name + "%"}
     ).fetchall()
-    print(results)
     if results is None:
         return make_response(jsonify({'error': 'Model evaluation record Not found'}), 404)
 
-    return jsonify({'results': results})
+    return json.dumps(marshal(results, resource_fields))
--- flask_backend/flaskr/schema.sql
+++ flask_backend/flaskr/schema.sql
@@ -1,13 +1,14 @@
-DROP TABLE IF EXISTS model_evalutation;
+DROP TABLE IF EXISTS model_evaluation;
 
-CREATE TABLE model_evalutation(
+CREATE TABLE model_evaluation(
     name TEXT PRIMARY KEY,
+    epoch INTEGER,
     exactMatch FLOAT NOT NULL,
     f1 FLOAT NOT NULL,
     loss FLOAT NOT NULL
 );
 
-INSERT INTO model_evalutation VALUES('BiDAF_epoch1', 11.688,24.202, 425.050);
-INSERT INTO model_evalutation VALUES('BiDAF_epoch2', 24.646,41.789, 328.571);
-INSERT INTO model_evalutation VALUES('BiDAF_epoch3', 36.463,57.326, 258.688);
-INSERT INTO model_evalutation VALUES('BiDAF_epoch4', 39.035,60.373, 243.138);
+INSERT INTO model_evaluation VALUES('BiDAF_1', 1, 11.688,24.202, 425.050);
+INSERT INTO model_evaluation VALUES('BiDAF_2', 2, 24.646,41.789, 328.571);
+INSERT INTO model_evaluation VALUES('BiDAF_3', 3, 36.463,57.326, 258.688);
+INSERT INTO model_evaluation VALUES('BiDAF_4', 4, 39.035,60.373, 243.138);
