00f5fddbded0ead8fe7419e1eaeac3bdbb26ddf1
WuXuan374 <2953098003@qq.com>
2021-05-17 09:51:43 +0800

    model Detail

--- flask_backend/flaskr/apis/model.py
+++ flask_backend/flaskr/apis/model.py
@@ -4,14 +4,25 @@ import json
 from flask_restplus import fields, marshal
 
 bp = Blueprint('model', __name__, url_prefix='/model')
-resource_fields = {
+
+info_fields = {
+    'name': fields.String,
+    'dataset': fields.String,
+    'word_dimension': fields.Integer,
+    'batch_size': fields.Integer,
+    'character_dimension': fields.Integer,
+    'dropout_rate': fields.Float,
+    'learning_rate': fields.Float,
+    'context_len': fields.Integer
+}
+
+evaluation_fields = {
     'name': fields.String,
     'epoch': fields.Integer,
     'exactMatch': fields.Float,
     'f1': fields.Float,
     'loss': fields.Float
 }
-
 
 
 @bp.route('/evaluation/', methods=['GET'])
@@ -24,4 +35,17 @@ def model_evaluation():
     if results is None:
         return make_response(jsonify({'error': 'Model evaluation record Not found'}), 404)
 
-    return json.dumps(marshal(results, resource_fields))
+    return json.dumps(marshal(results, evaluation_fields))
+
+@bp.route('/info/', methods=['GET'])
+def model_info():
+    db = get_db()
+    model_name = request.args.get("model_name")
+    result = db.execute(
+        "SELECT * FROM model_info WHERE name LIKE :search", {"search":"%" + model_name + "%"}
+    ).fetchone()
+
+    if result is None:
+        return make_response(jsonify({'error': 'Model info of {}  Not found'.format(model_name)}), 404)
+
+    return json.dumps(marshal(result,info_fields))
--- flask_backend/flaskr/schema.sql
+++ flask_backend/flaskr/schema.sql
@@ -12,3 +12,20 @@ INSERT INTO model_evaluation VALUES('BiDAF_1', 1, 11.688,24.202, 425.050);
 INSERT INTO model_evaluation VALUES('BiDAF_2', 2, 24.646,41.789, 328.571);
 INSERT INTO model_evaluation VALUES('BiDAF_3', 3, 36.463,57.326, 258.688);
 INSERT INTO model_evaluation VALUES('BiDAF_4', 4, 39.035,60.373, 243.138);
+
+DROP TABLE IF EXISTS model_info;
+
+CREATE TABLE model_info(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    dataset TEXT,
+    word_dimension INTEGER,
+    batch_size INTEGER,
+    character_dimension INTEGER,
+    dropout_rate FLOAT,
+    learning_rate FLOAT,
+    context_len INTEGER
+);
+
+INSERT INTO model_info (name, dataset, word_dimension, batch_size, character_dimension, dropout_rate, learning_rate, context_len)
+VALUES('BiDAF', 'SQuAD', 100, 50, 8, 0.2, 0.5, 150);
